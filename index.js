b200adb0b2a0377d5a38083a136500654a69e08b
nitin <nitinghimire.w@gmail.com>
2025-08-08 10:37:25 +0545

    Add function to get random choice of rock, paper or scissors

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 console.log("Hello World!");
 
+function getComputerChoice() {
+    let choiceNumber = getRandomChoice()
+    let choice
+    if (choiceNumber == 0) {
+        choice="Rock"
+    }
+    else if (choiceNumber==1) {
+        choice="Scissors"
+    }
+    else {
+        choice="Paper"
+    }
+    return choice
+}
+
+function getRandomChoice() {
+    return Math.floor((2.99 * Math.random()))    
+}
+
+console.log(getComputerChoice())
