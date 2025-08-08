d4aa5999c0ae3b8d6287b51db8df7797625afdf0
nitin <nitinghimire.w@gmail.com>
2025-08-08 10:56:19 +0545

    Add function to get human choice for the game

--- index.js
+++ index.js
@@ -19,4 +19,22 @@ function getRandomChoice() {
     return Math.floor((2.99 * Math.random()))    
 }
 
-console.log(getComputerChoice())
+
+function getHumanChoice() {
+    let humanChoice = prompt("Pick rock, paper or scissors:")
+    if (humanChoice.toLowerCase() == "rock") {
+        return humanChoice
+    }
+    else if (humanChoice.toLowerCase() == "paper") {
+        return humanChoice
+    }
+    else if (humanChoice.toLowerCase() == "scissor" || humanChoice.toLowerCase() == "scissors") {
+        return humanChoice
+    }
+    else {
+        alert("Sorry invalid Input Please choose a Valid Option!")
+    }
+}
+
+let play=prompt("Want to play a game of Rock, Paper and Scissors?")
+console.log(getHumanChoice())
